d26d6b68f984a44e3f22dc716b6b764be85120c0
Daniel Costa <daniel.dc697@gmail.com>
2025-04-16 10:46:48 -0300

    Adicionando a fonto no html e adicionando o estilo global na aplicação inteira

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,28 @@
+// src/styles/GlobalStyles.js
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  /* Reset básico */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  /* Fonte padrão */
+  body {
+    font-family: "Inter", sans-serif;
+
+  }
+
+  a {
+    text-decoration: none;
+
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyle;
