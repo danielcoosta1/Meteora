a831d501cf293e5e634fae37f6896dede18171b7
Daniel Costa <daniel.dc697@gmail.com>
2025-04-16 14:04:38 -0300

    Estilização do lado direto da BarraDeNavegacao - Campo de pesquisa e o icone do carrinho

--- src/components/BarraDeNavegacao/styles.js
+++ src/components/BarraDeNavegacao/styles.js
@@ -1,35 +1,72 @@
 import styled from "styled-components";
 
 const NavEstilizada = styled.nav`
-display: flex;
-justify-content: space-around;
-padding: 2rem 0;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 2rem 0;
+  background-color: #000000;
 `;
 
 const ConteinerEsquerdaEstilizado = styled.section`
-display: flex;
-
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
-const ConteinerDireitoEstilizado = styled.section``;
+const ConteinerDireitoEstilizado = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+`;
 
 const CampoSearch = styled.div`
-display: flex;
-gap: 1rem;
-align-items: center;
+  display: flex;
+  gap: .25rem;
+  align-items: center;
+ 
+
+  button{
+    padding:1rem;
+    background-color: transparent;
+    color: #ffffff;
+    border: 1px solid #FFFFFF;
+  }
 `;
 
-const CampoCart = styled.div``;
+const InputEstilizado = styled.input`
+  background: #ffffff;
+  border: 1px solid #ced4da;
+  color: #ced4da;
+  width: 60%;
+  padding: 1rem ;
+
+
+
+  &::placeholder {
+    color: #6c757d;
+    
+  }
+`;
+
+const CampoCart = styled.div`
+  display: flex;
+  align-items: center;
+
+  p {
+    color: #daff01;
+  }
+`;
 
 const ListaEstilizada = styled.ul`
-display: flex;
-gap: 1rem;
-
+  display: flex;
+  gap: 1rem;
 `;
 
 const ImgEstilizada = styled.img``;
 
-const InputEstilizado = styled.input``;
+
 
 export {
   ImgEstilizada,
@@ -39,5 +76,5 @@ export {
   ListaEstilizada,
   ConteinerDireitoEstilizado,
   CampoCart,
-  CampoSearch
+  CampoSearch,
 };
