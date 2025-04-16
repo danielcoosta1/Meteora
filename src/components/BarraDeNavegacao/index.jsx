448cbc1af530f1ff4e10d36a68b373cc5b6886fe
Daniel Costa <daniel.dc697@gmail.com>
2025-04-16 13:26:18 -0300

    Criando a base da BarrraDeNavegacao

---
+++ src/components/BarraDeNavegacao/index.jsx
@@ -0,0 +1,25 @@
+
+const BarraDeNavegacao = () => {
+    return (
+        <nav>
+            <h1>
+                Minha logo
+            </h1>
+            <ul>
+                <li>
+                    <a>Nossas lojas</a>
+                </li>
+                <li>
+                    <a>Novidades</a>
+                </li><li>
+                    <a>Promoções</a>
+                </li>
+            </ul>
+            <input></input>
+            <button>Buscar</button>
+
+        </nav>
+    )
+}
+
+export default BarraDeNavegacao
