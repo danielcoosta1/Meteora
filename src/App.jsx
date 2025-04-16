6ea4b826b5780f025e7a0b912d0d4f60cb28a071
Daniel Costa <daniel.dc697@gmail.com>
2025-04-16 10:47:12 -0300

    Iniciando o react-router com as 3 paginas que irão fazer parte da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+ 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Página Inicial</h1>
+      <p>Bem-vindo à página de compras!</p>
+    </div>
+  );
+};
+
+export default Home;
