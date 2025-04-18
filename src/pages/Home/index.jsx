babf8a22f27c8ddc7d83f619f3a4f786ecc4a97f
Daniel Costa <daniel.dc697@gmail.com>
2025-04-18 12:34:07 -0300

    Criei o estado para carrinho. Nele está contido um objeto com os itens que foram selecionados para compra. Assim, poderemos passar para o menu de navegação(que será usado no menu de barra lateral e no icone de quantidade do carrinho de compras)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,40 @@
 // src/pages/Home.jsx
 import React from "react";
+import { useState } from "react";
 import BarraDeNavegacao from "../../components/BarraDeNavegacao";
 import BannerCarrossel from "../../components/BannerCarrossel";
 import Categorias from "../../components/Categoria";
 import Produtos from "../../components/Produtos";
 
 const Home = () => {
+  const [carrinho, setCarrinho] = useState([]);
+
+  // Função para adicionar um produto ao carrinho
+  const adicionarAoCarrinho = (produto) => {
+    setCarrinho((prevCarrinho) => {
+      // Checa se o produto já está no carrinho
+      const produtoExistente = prevCarrinho.find(
+        (item) => item.id === produto.id
+      );
+      if (produtoExistente) {
+        // Se já existir, incrementa a quantidade
+        return prevCarrinho.map((item) =>
+          item.id === produto.id
+            ? { ...item, quantidade: item.quantidade + 1 }
+            : item
+        );
+      } else {
+        // Se não existir, adiciona com quantidade 1
+        return [...prevCarrinho, { ...produto, quantidade: 1 }];
+      }
+    });
+  };
   return (
     <>
-      <BarraDeNavegacao/>
-      <BannerCarrossel/>
-      <Categorias/>
-      <Produtos/>
+      <BarraDeNavegacao carrinho={carrinho} />
+      <BannerCarrossel />
+      <Categorias />
+      <Produtos adicionarAoCarrinho={adicionarAoCarrinho} />
     </>
   );
 };
