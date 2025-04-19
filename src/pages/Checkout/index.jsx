d63c5dd11ea695024cee0bc3147d473d3ca3dee4
Daniel Costa <daniel.dc697@gmail.com>
2025-04-19 14:37:05 -0300

    Estilizando a  renderização de quando o carrinho está vazio.

--- src/pages/Checkout/index.jsx
+++ src/pages/Checkout/index.jsx
@@ -3,7 +3,13 @@
 import { FaTrash } from "react-icons/fa";
 import BarraDeNavegacao from "../../components/BarraDeNavegacao";
 import imgBannerCarrinho from "/assets/images/banner-carrinho.png";
-import { BannerCheckout, ConteinerPrincipal } from "./styles";
+import {
+  BannerCheckout,
+  ConteinerPrincipal,
+  ConteinerVazio,
+  DescricaoSemProduto,
+} from "./styles";
+import VoltarHome from "../../components/VoltarHome";
 
 const Checkout = ({ carrinho, setCarrinho }) => {
   const carrinhoVazio = carrinho.length === 0;
@@ -17,7 +23,12 @@ const Checkout = ({ carrinho, setCarrinho }) => {
 
       <ConteinerPrincipal>
         {carrinhoVazio ? (
-          <p>Seu carrinho está vazio 😕</p>
+          <ConteinerVazio>
+            <DescricaoSemProduto>
+              Seu carrinho está vazio 😕
+            </DescricaoSemProduto>
+           <VoltarHome>Continuar compras</VoltarHome>
+          </ConteinerVazio>
         ) : (
           <p>Você tem {carrinho.length} item(ns) no carrinho!</p>
         )}
--- src/pages/Checkout/styles.js
+++ src/pages/Checkout/styles.js
@@ -11,5 +11,38 @@ export const BannerCheckout = styled.div`
 
 
 
-export const ConteinerPrincipal = styled.main``;
+export const ConteinerPrincipal = styled.main`
+  padding: 2rem;
+  min-height: 60vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.25rem;
+  background-color: #000000;
+  border: none;
+  margin: 5rem 0;
+  color: #ffffff;
+  font-size: 2rem;
+
+`;
+
+export const ConteinerVazio = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+
+button{
+    width: 60%;
+    align-item;
+    background-color: #9353FF;
+    color: #ffffff;
+
+}
+
+`;
+
+export const DescricaoSemProduto = styled.p``;
+
+
 
