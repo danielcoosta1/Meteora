330fe17578826bf6b15bd905010cc7099df2ec48
Daniel Costa <daniel.dc697@gmail.com>
2025-04-20 17:46:05 -0300

    retirando a margin-top do componente VoltarHome

--- src/components/VoltarHome/styles.js
+++ src/components/VoltarHome/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ButtonEstilizado = styled.button`
-    margin-top: 2rem;
+
     padding: .75rem 1.5rem;
     border: 1px solid #000;
     cursor: pointer;
