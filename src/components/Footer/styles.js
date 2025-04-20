60690bcf5885779e6bae6d59c9807092798fbda1
Daniel Costa <daniel.dc697@gmail.com>
2025-04-20 19:43:27 -0300

    atualizando a estilização do footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,19 +1,10 @@
 import styled from "styled-components";
 
 export const Rodape = styled.footer`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 100%;
-background-color: #000000;
-color: #ffffff;
-padding: 2rem auto;
-text-align: center;
-min-height: 4rem;
-
-p{
-    font-weight: 400;
-}
-
+  text-align: center;
+  padding: 2rem 1rem;
+  background-color:#000000;
+  color: #ffffff;
+  font-size: 0.875rem;
 `;
 
