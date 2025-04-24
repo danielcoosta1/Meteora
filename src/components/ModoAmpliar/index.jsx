090246a566423fc4a29758594aa90a97f72e3a8d
Daniel Costa <daniel.dc697@gmail.com>
2025-04-24 14:03:21 -0300

    Baixei o famer-motion do react, para colocar uma animação no ModalAmpliar

--- src/components/ModoAmpliar/index.jsx
+++ src/components/ModoAmpliar/index.jsx
@@ -1,6 +1,9 @@
 import { useCarrinho } from "../../hooks/useCarrinho";
 import { FaTimes, FaHeart, FaRegHeart } from "react-icons/fa";
 import { useFavoritos } from "../../hooks/useFavoritos";
+
+import { motion, AnimatePresence } from "framer-motion";
+
 import {
   BotaoCarrinho,
   ConteinerBotoes,
@@ -30,33 +33,52 @@ const ModoAmpliar = () => {
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, []);
+  }, [fecharModal]);
 
   return (
-    <ModalOverlay onClick={fecharModal}>
-      <ModalContent onClick={(e) => e.stopPropagation()}>
-        <ModalClose onClick={fecharModal}>
-          <FaTimes />
-        </ModalClose>
-        <img src={produtoAmpliado.src} alt={produtoAmpliado.alt} />
-        <TituloProduto>{produtoAmpliado.titulo}</TituloProduto>
-        <DescricaoProduto>{produtoAmpliado.descricao}</DescricaoProduto>
-        <PrecoProduto>R$ {produtoAmpliado.preco}</PrecoProduto>
-        <ConteinerBotoes>
-          <BotaoCarrinho onClick={() => adicionarAoCarrinho(produtoAmpliado)}>
-            Adicionar ao carrinho
-          </BotaoCarrinho>
-          <div>
-            <IconesWrapper
-              onClick={() => handleFavoritarProduto(produtoAmpliado)}
-              $favoritado={favoritado}
-            >
-              {favoritado ? <FaHeart /> : <FaRegHeart />}
-            </IconesWrapper>
-          </div>
-        </ConteinerBotoes>
-      </ModalContent>
-    </ModalOverlay>
+    <AnimatePresence>
+      {produtoAmpliado && (
+        <ModalOverlay
+          as={motion.div}
+          onClick={fecharModal}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <ModalContent
+            as={motion.div}
+            onClick={(e) => e.stopPropagation()}
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.8, opacity: 0 }}
+            transition={{ type: "spring", stiffness: 300, damping: 30 }}
+          >
+            <ModalClose onClick={fecharModal}>
+              <FaTimes />
+            </ModalClose>
+            <img src={produtoAmpliado.src} alt={produtoAmpliado.alt} />
+            <TituloProduto>{produtoAmpliado.titulo}</TituloProduto>
+            <DescricaoProduto>{produtoAmpliado.descricao}</DescricaoProduto>
+            <PrecoProduto>R$ {produtoAmpliado.preco}</PrecoProduto>
+            <ConteinerBotoes>
+              <BotaoCarrinho
+                onClick={() => adicionarAoCarrinho(produtoAmpliado)}
+              >
+                Adicionar ao carrinho
+              </BotaoCarrinho>
+              <div>
+                <IconesWrapper
+                  onClick={() => handleFavoritarProduto(produtoAmpliado)}
+                  $favoritado={favoritado}
+                >
+                  {favoritado ? <FaHeart /> : <FaRegHeart />}
+                </IconesWrapper>
+              </div>
+            </ConteinerBotoes>
+          </ModalContent>
+        </ModalOverlay>
+      )}
+    </AnimatePresence>
   );
 };
 
