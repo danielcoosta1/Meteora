717c6500bd4ee4310b449373c526a27f5bffc533
Daniel Costa <daniel.dc697@gmail.com>
2025-04-25 16:32:51 -0300

    Limpei o código da pagina produtos, retirando toda a lógica e deixando apenas o que se refere a interface, recebendo o restante por provider(hook)

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -15,55 +15,33 @@ import {
 import { MdCheckroom } from "react-icons/md";
 
 import { FaHeart, FaRegHeart, FaExpand } from "react-icons/fa";
-import todosProdutos from "../../mocks/todosProdutos.json";
+
 import Categorias from "../../components/Categoria";
 import { useCarrinho } from "../../hooks/useCarrinho";
 import BarraDeNavegacao from "../../components/BarraDeNavegacao";
-
-
 
 import MenuLateralCarrinho from "../../components/MenuLateralCarrinho";
 import { useFavoritos } from "../../hooks/useFavoritos";
 import ModoAmpliar from "../../components/ModoAmpliar";
 import TituloSecao from "../../components/TituloDefaultSecao";
+
+import { useProdutos } from "../../hooks/useProdutos";
 const Produtos = () => {
+    
   const { handleFavoritarProduto, isFavoritado } = useFavoritos();
-  const {
-    produtoAmpliado,
-    abrirModal,
-    categoriaSelecionada,
-    termoBusca,
-    menuAberto,
-    adicionarAoCarrinho,
-  } = useCarrinho(); // Importa o hook de categorias
-  // Filtra os produtos com base na categoria selecionada e no termo de busca
-  const produtosFiltradosPorCategoria = categoriaSelecionada
-    ? todosProdutos.filter(
-        (produto) =>
-          //Se a categoria do produto for igual a categoria selecionada, retorna o produto
-          produto.categoria.toLowerCase() === categoriaSelecionada.toLowerCase()
-      )
-    : todosProdutos; // Se não houver categoria selecionada, retorna todos os produtos
-
-  // Filtra os produtos com base no termo de busca
-  const produtosFiltradosPorBusca = termoBusca
-    ? produtosFiltradosPorCategoria.filter((produto) =>
-        produto.titulo.toLowerCase().includes(termoBusca.toLowerCase())
-      )
-    : produtosFiltradosPorCategoria;
-
-  // Se houver categoria selecionada ou termo de busca, exibe os produtos filtrados, caso contrário, exibe os produtos bombando
-  const produtosParaExibir =
-    categoriaSelecionada || termoBusca
-      ? produtosFiltradosPorBusca
-      : todosProdutos;
+  const { adicionarAoCarrinho, menuAberto } = useCarrinho(); // Importa o hook de categorias
+  const { abrirModal, produtoAmpliado, produtosParaExibir } = useProdutos(); // Importa o hook de carrinho
 
   return (
     <>
       <BarraDeNavegacao />
       <ContainerProdutos>
         <Categorias />
-        <TituloSecao texto="Todos os produtos dísponíveis" Icone={MdCheckroom}/>
+
+        <TituloSecao
+          texto="Todos os produtos dísponíveis"
+          Icone={MdCheckroom}
+        />
         <GridProdutos>
           {produtosParaExibir.map((produto) => (
             <Card key={produto.id}>
