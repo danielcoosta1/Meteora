16a7a218a810b256c0ba2ecc585950faa408af32
Daniel Costa <daniel.dc697@gmail.com>
2025-04-25 09:30:15 -0300

    Produtos bombando é um componente mais simples, apenas mostra o JSON de produtos bobmando

--- src/components/ProdutosBombando/index.jsx
+++ src/components/ProdutosBombando/index.jsx
@@ -6,7 +6,6 @@ import {
   ConteinerTitulo,
   DescricaoProduto,
   GridProdutos,
-  IconeFiltrar,
   IconesWrapper,
   ImagemProduto,
   PrecoProduto,
@@ -19,69 +18,24 @@ import { useCarrinho } from "../../hooks/useCarrinho";
 
 import { useFavoritos } from "../../hooks/useFavoritos";
 
-import { BiEraser } from "react-icons/bi";
 import { FaHeart, FaRegHeart, FaExpand } from "react-icons/fa";
 
 import produtosBombando from "../../mocks/produtosBombando.json";
 import ModoAmpliar from "../ModoAmpliar";
 
-const Produtos = ({ todosProdutos }) => {
-  const {
-    categoriaSelecionada,
-    setCategoriaSelecionada,
-    termoBusca,
-    adicionarAoCarrinho,
-    produtoAmpliado,
-    abrirModal,
-  } = useCarrinho();
+const ProdutosBombando = () => {
+  const { adicionarAoCarrinho, produtoAmpliado, abrirModal } = useCarrinho();
 
   const { handleFavoritarProduto, isFavoritado } = useFavoritos();
-
-  // Filtra os produtos com base na categoria selecionada e no termo de busca
-  const produtosFiltradosPorCategoria = categoriaSelecionada
-    ? todosProdutos.filter(
-        (produto) =>
-          //Se a categoria do produto for igual a categoria selecionada, retorna o produto
-          produto.categoria.toLowerCase() === categoriaSelecionada.toLowerCase()
-      )
-    : todosProdutos; // Se não houver categoria selecionada, retorna todos os produtos
-
-  // Filtra os produtos com base no termo de busca
-  const produtosFiltradosPorBusca = termoBusca
-    ? produtosFiltradosPorCategoria.filter((produto) =>
-        produto.titulo.toLowerCase().includes(termoBusca.toLowerCase())
-      )
-    : produtosFiltradosPorCategoria;
-
-  // Se houver categoria selecionada ou termo de busca, exibe os produtos filtrados, caso contrário, exibe os produtos bombando
-  const produtosParaExibir =
-    categoriaSelecionada || termoBusca
-      ? produtosFiltradosPorBusca
-      : produtosBombando;
 
   return (
     <SecaoProdutos>
       <ConteinerTitulo>
-        <Titulo>
-          {termoBusca
-            ? `Resultados para: "${termoBusca}"`
-            : categoriaSelecionada
-            ? `Exibindo: ${categoriaSelecionada}`
-            : "Produtos que estão bombando!"}
-        </Titulo>
-        {categoriaSelecionada && (
-          <IconeFiltrar onClick={() => setCategoriaSelecionada(null)}>
-            <BiEraser
-              size={20}
-              style={{ marginRight: "8px", color: " #9353ff" }}
-            />
-            Limpar filtro
-          </IconeFiltrar>
-        )}
+        <Titulo>Produtos que estão bombando!</Titulo>
       </ConteinerTitulo>
 
       <GridProdutos>
-        {produtosParaExibir.map((produto) => (
+        {produtosBombando.map((produto) => (
           <CardProduto key={produto.id}>
             <ImagemProduto src={produto.src} alt={produto.alt} />
             <TituloProduto>{produto.titulo}</TituloProduto>
@@ -106,11 +60,9 @@ const Produtos = ({ todosProdutos }) => {
           </CardProduto>
         ))}
       </GridProdutos>
-      {produtoAmpliado && (
-        <ModoAmpliar/>
-      )}
+      {produtoAmpliado && <ModoAmpliar />}
     </SecaoProdutos>
   );
 };
 
-export default Produtos;
+export default ProdutosBombando;
