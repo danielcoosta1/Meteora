7809f5b549d21a7f35f752800cd43292663de860
Daniel Costa <daniel.dc697@gmail.com>
2025-04-26 11:15:48 -0300

    Utilização do useMemo nas variaveis relacionadas ao filtro de produtos

--- src/context/produtos/ProdutosProvider.jsx
+++ src/context/produtos/ProdutosProvider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { ProdutosContext } from "./ProdutosContext";
 
 import todosProdutos from "../../mocks/todosProdutos.json";
@@ -11,27 +11,31 @@ export const ProdutoProvider = ({ children }) => {
   const abrirModal = (produto) => setProdutoAmpliado(produto);
   const fecharModal = () => setProdutoAmpliado(null);
 
-  // Filtra os produtos com base na categoria selecionada e no termo de busca
-  const produtosFiltradosPorCategoria = categoriaSelecionada
-    ? todosProdutos.filter(
-        (produto) =>
-          //Se a categoria do produto for igual a categoria selecionada, retorna o produto
-          produto.categoria.toLowerCase() === categoriaSelecionada.toLowerCase()
-      )
-    : todosProdutos; // Se não houver categoria selecionada, retorna todos os produtos
+  // Filtra os produtos com base na categoria selecionada
+  // e no termo de busca
+  const produtosFiltradosPorCategoria = useMemo(() => { //useMemo é usado para memorizar o resultado da função, evitando cálculos desnecessários
+    return categoriaSelecionada
+      ? todosProdutos.filter(
+          (produto) =>
+            produto.categoria.toLowerCase() ===
+            categoriaSelecionada.toLowerCase()
+        )
+      : todosProdutos;
+  }, [categoriaSelecionada]);
 
-  // Filtra os produtos com base no termo de busca
-  const produtosFiltradosPorBusca = termoBusca
-    ? produtosFiltradosPorCategoria.filter((produto) =>
-        produto.titulo.toLowerCase().includes(termoBusca.toLowerCase())
-      )
-    : produtosFiltradosPorCategoria;
+  const produtosFiltradosPorBusca = useMemo(() => {
+    return termoBusca
+      ? produtosFiltradosPorCategoria.filter((produto) =>
+          produto.titulo.toLowerCase().includes(termoBusca.toLowerCase())
+        )
+      : produtosFiltradosPorCategoria;
+  }, [termoBusca, produtosFiltradosPorCategoria]);
 
-  // Se houver categoria selecionada ou termo de busca, exibe os produtos filtrados, caso contrário, exibe os produtos bombando
-  const produtosParaExibir =
-    categoriaSelecionada || termoBusca
+  const produtosParaExibir = useMemo(() => {
+    return categoriaSelecionada || termoBusca
       ? produtosFiltradosPorBusca
       : todosProdutos;
+  }, [categoriaSelecionada, termoBusca, produtosFiltradosPorBusca]);
 
   return (
     <ProdutosContext.Provider
