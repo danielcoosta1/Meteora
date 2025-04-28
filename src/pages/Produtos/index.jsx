81f6c08d15cfe5c69c03a905f23d0257a20ba094
Daniel Costa <daniel.dc697@gmail.com>
2025-04-28 17:17:13 -0300

    Da mesma forma fiz com a pagina de produtos, que recebe também os cards com as suas funcionalidades do componente generico ProdutoCard

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -1,17 +1,8 @@
 import {
-  BotaoCarrinho,
-  Card,
   ContainerProdutos,
   ContainerTitulo,
   ContainerWrapper,
-  ConteinerBotoes,
-  ConteinerIcones,
-  DescricaoProduto,
   GridProdutos,
-  IconesWrapper,
-  ImagemProduto,
-  PrecoProduto,
-  TituloProduto,
   FiltrosAplicados,
   MensagemNaoEncontrada,
   FiltroSpan,
@@ -19,26 +10,23 @@ import {
 } from "./styles";
 import { BiEraser } from "react-icons/bi";
 
-import { FaHeart, FaRegHeart, FaExpand } from "react-icons/fa";
-
 import Categorias from "../../components/Categoria";
 import { useCarrinho } from "../../hooks/useCarrinho";
 import BarraDeNavegacao from "../../components/BarraDeNavegacao";
 
 import MenuLateralCarrinho from "../../components/MenuLateralCarrinho";
-import { useFavoritos } from "../../hooks/useFavoritos";
+
 import ModoAmpliar from "../../components/ModoAmpliar";
 import TituloSecao from "../../components/TituloDefaultSecao";
 
 import { useProdutos } from "../../hooks/useProdutos";
 import BotaoLimpar from "../../components/BotaoLimpar";
 import AsiderFiltros from "../../components/AsiderFiltros";
+import ProdutoCard from "../../components/ProdutoCard";
 
 const Produtos = () => {
-  const { handleFavoritarProduto, isFavoritado } = useFavoritos();
-  const { adicionarAoCarrinho, menuAberto } = useCarrinho(); // Importa o hook de categorias
+  const { menuAberto } = useCarrinho(); // Importa o hook de categorias
   const {
-    abrirModal,
     produtoAmpliado,
     produtosParaExibir,
     haProdutosFiltrados,
@@ -88,7 +76,6 @@ const Produtos = () => {
           <Categorias />
           <ContainerTitulo>
             <TituloSecao texto={titulo} />
-
             {haProdutosFiltrados && (
               <BotaoLimpar
                 onClick={limparFiltro}
@@ -107,28 +94,7 @@ const Produtos = () => {
               </MensagemNaoEncontrada>
             ) : (
               produtosParaExibir.map((produto) => (
-                <Card key={produto.id}>
-                  <ImagemProduto src={produto.src} alt={produto.alt} />
-                  <TituloProduto>{produto.titulo}</TituloProduto>
-                  <DescricaoProduto>{produto.descricao}</DescricaoProduto>
-                  <PrecoProduto>R$ {produto.preco.toFixed(2)}</PrecoProduto>
-                  <ConteinerBotoes>
-                    <BotaoCarrinho onClick={() => adicionarAoCarrinho(produto)}>
-                      Adicionar ao carrinho
-                    </BotaoCarrinho>
-                    <ConteinerIcones>
-                      <IconesWrapper
-                        onClick={() => handleFavoritarProduto(produto)}
-                        $favoritado={isFavoritado(produto)}
-                      >
-                        {isFavoritado(produto) ? <FaHeart /> : <FaRegHeart />}
-                      </IconesWrapper>
-                      <IconesWrapper onClick={() => abrirModal(produto)}>
-                        <FaExpand />
-                      </IconesWrapper>
-                    </ConteinerIcones>
-                  </ConteinerBotoes>
-                </Card>
+                <ProdutoCard key={produto.id} produto={produto} />
               ))
             )}
           </GridProdutos>
