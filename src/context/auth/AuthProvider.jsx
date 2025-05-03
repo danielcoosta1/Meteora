b58547113617fba72e0cd91b5d8f49c3b12619ef
Daniel Costa <daniel.dc697@gmail.com>
2025-05-03 10:18:01 -0300

    Criei um contexto para o usuario

---
+++ src/context/auth/AuthProvider.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { AuthContext } from "./AuthContext";
+
+export const AuthProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState(null);
+
+  useEffect(() => {
+    const usuarioSalvo = localStorage.getItem("usuario");
+    if (usuarioSalvo) {
+      setUsuario(JSON.parse(usuarioSalvo));
+    }
+  }, []);
+
+  const login = (dadosUsuario) => {
+    setUsuario(dadosUsuario);
+    localStorage.setItem("usuario", JSON.stringify(dadosUsuario));
+  };
+
+  const logout = () => {
+    setUsuario(null);
+    localStorage.removeItem("usuario");
+  };
+
+  return (
+    <AuthContext.Provider value={{ usuario, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
