dbb034490d1e0cff3f48af6adaab6191fec52d64
Daniel Costa <daniel.dc697@gmail.com>
2025-05-03 12:07:35 -0300

    Criei um componente para poder passar o estado do menuAberto para toda a aplicação, sendo possivel usar o icone do carrinho sem precisar utilizar a logica do menu e menuLateral em toda pagina.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import Home from "./pages/Home";
-
-import Checkout from "./pages/Checkout";
-import Error from "./pages/Error404";
-
-import Novidades from "./pages/Novidades";
-import Promocoes from "./pages/Promocoes";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { CarrinhoProvider } from "./context/carrinho/CarrinhoProvider";
 import { FavoritosProvider } from "./context/favoritos/FavoritosProvider";
 import { ProdutoProvider } from "./context/produtos/ProdutosProvider";
 import { AuthProvider } from "./context/auth/AuthProvider";
+import AppContent from "./components/AppContent";
 
-import Favoritos from "./pages/Favoritos";
-
-import Produtos from "./pages/Produtos";
-import Login from "./pages/Login";
 
 function App() {
   return (
@@ -25,25 +14,7 @@ function App() {
         <ProdutoProvider>
           <FavoritosProvider>
             <CarrinhoProvider>
-              <ToastContainer
-                position="top-right"
-                autoClose={2000}
-                hideProgressBar={false}
-                closeOnClick
-                pauseOnHover
-                draggable
-                theme="colored"
-              />
-              <Routes>
-                <Route path="/login" element={<Login/>} />
-                <Route path="/produtos" element={<Produtos />} />
-                <Route path="/promocoes" element={<Promocoes />} />
-                <Route path="/novidades" element={<Novidades />} />
-                <Route path="/" element={<Home />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/favoritos" element={<Favoritos />} />
-                <Route path="*" element={<Error />} />
-              </Routes>
+              <AppContent/>
             </CarrinhoProvider>
           </FavoritosProvider>
         </ProdutoProvider>
---
+++ src/components/AppContent/index.jsx
@@ -0,0 +1,47 @@
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import MenuLateralCarrinho from "../MenuLateralCarrinho";
+
+import Home from "../../pages/Home";
+import Produtos from "../../pages/Produtos";
+import Login from "../../pages/Login";
+import Checkout from "../../pages/Checkout";
+import Error from "../../pages/Error404";
+import Novidades from "../../pages/Novidades";
+import Promocoes from "../../pages/Promocoes";
+import Favoritos from "../../pages/Favoritos";
+import { useCarrinho } from "../../hooks/useCarrinho";
+
+const AppContent = () => {
+  const { menuAberto } = useCarrinho();
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
+
+      {menuAberto && <MenuLateralCarrinho />}
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/produtos" element={<Produtos />} />
+        <Route path="/promocoes" element={<Promocoes />} />
+        <Route path="/novidades" element={<Novidades />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppContent;
