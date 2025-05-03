bf93fc61e1057b713d4588962dbcc38dc8d38abc
Daniel Costa <daniel.dc697@gmail.com>
2025-05-03 15:45:27 -0300

    Coloquei apenas o estado inicial como um array vazio e todo a manipulação, logica e funcionalidade será feita no CarrinhoProvider

--- src/context/Carrinho/initialState.js
+++ src/context/Carrinho/initialState.js
@@ -1,12 +1,4 @@
-// src/context/carrinho/initialState.js
-import { localStorageService } from "../../services/localStorageService";
-
-const carrinhoLocal =
-  typeof window !== "undefined"
-    ? localStorageService.ler("carrinho") || []
-    : [];
-
 export const initialState = {
-  carrinho: carrinhoLocal,
+  carrinho: [],
   menuAberto: false,
 };
