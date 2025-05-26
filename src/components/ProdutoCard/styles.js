2f92626104b3e2ccb4b40335ff70e0e25c412032
Daniel Costa <daniel.dc697@gmail.com>
2025-05-26 10:49:19 -0300

    Responsividade do card generico

--- src/components/ProdutoCard/styles.js
+++ src/components/ProdutoCard/styles.js
@@ -10,30 +10,64 @@ const CardProduto = styled.div`
   &:hover {
     transform: scale(1.03);
   }
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 1rem 0.5rem;
+  }
+
+  @media (max-width: 480px) {
+    padding: 0.5rem;
+
+  }
 `;
 
 const ImagemProduto = styled.img`
   width: 100%;
-  
-  height: 350px;
-  object-fit: cover;
+  height: auto;
+
   border-radius: 8px;
 `;
 
 const TituloProduto = styled.h3`
   margin: 1rem 0 0.5rem;
   font-size: 1.2rem;
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 0.75rem;
+  }
 `;
 
 const DescricaoProduto = styled.p`
   font-size: 0.9rem;
   color: #555;
   margin-bottom: 1.5rem;
+
+  @media (max-width: 768px) {
+    font-size: 0.75rem;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 0.8rem;
+  }
 `;
 
 const PrecoProduto = styled.p`
   font-weight: bold;
   margin: 0.5rem 0;
+
+  font-size: 1.1rem;
+
+  @media (max-width: 768px) {
+    font-size: 0.9rem;
+  }
+  @media (max-width: 480px) {
+    font-size: 0.8rem;
+  }
 `;
 
 const BotaoCarrinho = styled.button`
@@ -46,6 +80,13 @@ const BotaoCarrinho = styled.button`
   &:hover {
     opacity: 0.9;
   }
+
+  @media (max-width: 768px) {
+    font-size: 0.85rem;
+  }
+  @media (max-width: 600px) {
+    font-size: 0.75rem;
+  }
 `;
 
 const ConteinerBotoes = styled.div`
@@ -56,6 +97,11 @@ const ConteinerBotoes = styled.div`
 
 const ConteinerIcones = styled.div`
   display: flex;
+  gap: 1rem;
+
+  @media (max-width: 768px) {
+    gap: 0;
+  }
 `;
 
 const IconesWrapper = styled.button`
@@ -80,6 +126,25 @@ const IconesWrapper = styled.button`
   svg {
     width: 20px;
     height: 20px;
+  }
+
+  @media (max-width: 768px) {
+    width: 30px;
+    height: 35px;
+
+    svg {
+      width: 18px;
+      height: 18px;
+    }
+  }
+  @media (max-width: 480px) {
+    width: 30px;
+    height: 30px;
+
+    svg {
+      width: 16px;
+      height: 16px;
+    }
   }
 `;
 
