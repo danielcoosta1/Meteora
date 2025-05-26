80735ce40a8cf22a44dcf5aa2353d75e1a65cc17
Daniel Costa <daniel.dc697@gmail.com>
2025-05-26 14:08:31 -0300

    Responsividade do Home - part1

--- src/components/ProdutosBombando/styles.js
+++ src/components/ProdutosBombando/styles.js
@@ -6,9 +6,16 @@ const SecaoProdutos = styled.section`
 
 const GridProdutos = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   gap: 2rem;
   margin-bottom: 5rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
+  }
+  @media (max-width: 480px) {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
 `;
 
 export { SecaoProdutos, GridProdutos };
